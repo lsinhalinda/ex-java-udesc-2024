d24fa1a9932c68ac00cc2bf68b8233edf92fb267
Isa <souzaisaihd@gmail.com>
2024-09-09 19:49:32 -0300

    Commit media aluno

---
+++ src/exercicios1/Ex9.java
@@ -0,0 +1,32 @@
+package exercicios1;
+import java.util.Scanner;
+public class Ex9 {
+    public static void main(String[] agr) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nome = ler.nextLine();
+
+        System.out.println("Digite sua primeira nota");
+        int nota1 = ler.nextInt();
+
+        System.out.println("Digite sua segunda nota");
+        int nota2 = ler.nextInt();
+
+        System.out.println("Digite sua terceira nota");
+        int nota3 = ler.nextInt();
+
+        double media = nota1+nota2+nota3;
+
+        if(media>=7) {
+            System.out.printf("%s esta aprovado", nome);
+        }
+        if(media>=5 && media<7) {
+            System.out.printf("%s esta em exame", nome);
+        }
+        if(media<5){
+            System.out.printf("%s esta reprovado", nome);
+        }
+
+    }
+}
